9eec9e7a66a9c8da666f831ff794d157d5fed056
Nguyentnt <134568442+Nguyentnt@users.noreply.github.com>
2023-06-07 16:52:52 -0400

    Uploading code that provides 30-Year Mortgage and Interest rate along with Mortgage due date
    Code consist of calculations of three credit scores to determine the eligibility of the customer if qualify for the 30 year mortgage at a fixed rate.  Customer will then determine what first day payment will be

---
+++ Github Test/MortgageDueDay.java
@@ -0,0 +1,66 @@
+//importing the scanner class
+import java.util.Scanner;
+
+class MortgageDueDay {
+
+   public static void main(String[] args){
+
+// Creating a Scanner object to receive input //
+
+   Scanner input = new Scanner (System.in);
+
+/*Declaring variables of when first payment is due in 45 days and to 
+input day number of current day */
+
+   int dayNumber;
+   int firstPayment;
+   
+
+/*Receiving input from user to input the day number of current day and 
+duration till the first payment due*/
+
+   System.out.print("Please enter the day number in a week for today.");
+   System.out.print("(Make sure you enter 0 for Sunday, 1 for Monday, 2 for Tuesday,... and 6 for Saturday.):");
+   dayNumber= input.nextInt();
+   
+   System.out.print("In how many days is the first payment due?(Make sure you enter an integer number like 10, 20, etc.):");
+   firstPayment= input.nextInt();
+   
+   
+/*Using modulo operation to calculate the day number of the week for the
+payment due date.*/
+
+   int dueDate = (dayNumber + firstPayment)%7;
+   
+/*Switch creation of statements that will use the calculations to determine what 
+day of the week will be due date*/
+   
+   switch (dueDate){
+      case 0:  
+         System.out.println("Your first payment is due on a Sunday.");
+         break;
+      case 1:  
+         System.out.println("Your first payment is due on a Monday.");
+         break;
+      case 2:  
+         System.out.println("Your first payment is due on a Tuesday.");
+         break;
+      case 3:  
+         System.out.println("Your first payment is due on a Wednesday.");
+         break;
+      case 4:  
+         System.out.println("Your first payment is due on a Thursday.");
+         break;
+      case 5:  
+         System.out.println("Your first payment is due on a Friday.");
+         break;
+      case 6:  
+         System.out.println("Your first payment is due on a Saturday.");
+      
+      
+      }
+   
+   
+   }
+
+}
